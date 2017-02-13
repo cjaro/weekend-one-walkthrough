867870432eb4868c879b6bfc6199fe16bfadb3af
Catherine J <cjarocki@gmail.com>
2017-02-13 09:51:42 -0600

    Adding new employee finctionality

---
+++ client.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){ //waits for DOM  (HTML & CSS) to completely load
+  $('#submitNewEmployee').on('click', function(){   // event listener on submitNewEmployee button
+    // declaring variables and retrieving values from input boxes
+    var firstName = $('#firstName').val();
+    var lastName = $('#lastName').val();
+    var idNumber = $('#idNumber').val();
+    var jobTitle = $('#jobTitle').val();
+    var annualSalary = $('#annualSalary').val();
+
+    // adds new employee row to DOM
+    $('#employeeTableBody').append(
+      '<tr>' +
+        '<td>' + firstName + '</td>' +
+        '<td>' + lastName + '</td>' +
+        '<td>' + idNumber + '</td>' +
+        '<td>' + jobTitle + '</td>' +
+        '<td>' + annualSalary + '</td>' +
+      '</tr>'
+    );
+  });
+});
